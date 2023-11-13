06ba7dc5903a5006ef52a996837d12c05c0c0ac6
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2023-11-13 18:41:37 +0800

    updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,26 +4,26 @@ import Link from 'next/link';
 
 const Sidebar = () => {
   return (
-    <aside className="w-64 h-screen p-4 flex flex-col items-center justify-center">
+    <aside className="w-64 h-screen p-4 ms-3 flex flex-col items-center justify-center">
       <nav>
         <ul className="space-y-4">
           <li>
             <Link href="/">
-              <h1 className="text-5xl hover:scale-110 transition duration-200">
+              <h1 className="text-6xl hover:scale-110 transition duration-200">
                 Home
               </h1>
             </Link>
           </li>
           <li>
             <Link href="/about">
-              <h1 className="text-5xl hover:scale-110 transition duration-200">
+              <h1 className="text-6xl hover:scale-110 transition duration-200">
                 About
               </h1>
             </Link>
           </li>
           <li>
             <Link href="/contact">
-              <h1 className="text-5xl hover:scale-110 transition duration-200">
+              <h1 className="text-6xl hover:scale-110 transition duration-200">
                 Contact
               </h1>
             </Link>
