e8ebca4adab4880061147c8d6b6d0ff867a129a6
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2023-11-22 12:43:00 +0800

    created button as component

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,17 +1,18 @@
-import Image from 'next/image'
 import Cursor from '@/components/Cursor'
+import BackButton from "@/components/BackButton"
 import './styles.css'
 import '@/app/styles/fonts.css'
-import WestOutlinedIcon from '@mui/icons-material/WestOutlined';
 
 export default function About() {
   return (
     <main className="flex min-h-screen min-w-screen flex-row justify-between p-24 border">
-      <Cursor />
-      <div className="basis-1/6 border">
-        <WestOutlinedIcon /> Back
+      {/* <Cursor /> */}
+      <div className="basis-1/6">
+        <div className="h-10 flex flex-row justify-center align-items-center">
+          <BackButton />
+        </div>
       </div>
-      <div className="basis-5/6 border">
+      <div className="basis-5/6">
         <h1 className="text-4xl font-extrabold">Contact</h1>
       </div>
     </main>
