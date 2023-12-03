2e2b0b51286cf90a23dbd579e3f9162eb7a06603
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2023-12-03 12:04:13 +0800

    updated about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,91 +1,59 @@
-import Image from 'next/image'
 import Cursor from '@/components/Cursor'
+import BackButton from "@/components/BackButton"
 import './styles.css'
 import '@/app/styles/fonts.css'
 
 export default function About() {
+  const position = [
+    'Software Developer',
+    'Web Developer',
+    'Apprentice • Industry Training Bootcamp',
+  ];
+  const employer = [
+    'Mulah Technologies',
+    'Freelance',
+    'Brainnest'
+  ];
+  const duration = [
+    'Aug 2023 - present',
+    'Mar 2023 - Jul 2023',
+    'Jan 2023 – Feb 2023',
+  ]
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen min-w-screen flex-row justify-between p-24 border">
       <Cursor />
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="basis-1/6">
+        <BackButton />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+      <div className="basis-5/6">
+        <div className='mb-10'>
+          <h1 className="text-4xl font-extrabold">About</h1>
+          <p className='pt-5 pe-10'>
+          I am a web developer with an interest in content creation,<br />
+          and I'm currently a software developer with Mulah Technologies.<br />
+          <br />
+          Initiallty a classically trained cellist,<br />
+          but I have always been passionate about digital technologies and architecture.<br />
+          A nick for details could be my middle name, but I much prefer to be called a foodie!<br />
+          <br />
+          In my work, I focus on interactibility and usability of websites.<br />
+          A good website should not only be aesthetic and pretty,<br />
+          but also accessible to different types of users.<br />
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </div>
+        <div>
+          <h1 className="text-4xl font-extrabold">Experience</h1>
+          {position.map((position, index) => (
+              <ul key={index} className='pt-4 pe-10'>
+                <li className="py-2 pe-4">
+                  {position}<br />
+                  {employer[index]}<br />
+                  {duration[index]}
+                </li>
+              </ul>
+            ))}
+        </div>
       </div>
     </main>
   )
